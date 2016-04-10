18a3dc63c438fa6eaa7eb2644c00a397bd5d7ecf
Andreas Madsack <andreas@madflex.de>
2016-04-10 16:40:49 +0200

    debugging

--- deploy.sh
+++ deploy.sh
@@ -8,13 +8,16 @@ REPO=${REPO/https:\/\/github.com\//git@github.com:}
 
 cp travis_ci_mlugs_website ~/.ssh/id_ed25519
 chmod 600 ~/.ssh/id_ed25519
-#eval `ssh-agent -s`
-#ssh-add
+eval `ssh-agent -s`
+ssh-add
 git config user.name "travis"
 git config user.email "travis"
 
 cd website
 pelican -ds publishconf.py .
+echo "-A-"
 echo "www.mlugs.de" > output/CNAME
+echo "-B-"
 ghp-import output
+echo "-C-"
 git push -f $REPO gh-pages
