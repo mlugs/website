117f688db67ae3fc8067774a7d2f601fab01c5b6
Andreas Madsack <andreas@madflex.de>
2016-04-10 16:25:47 +0200

    and another try

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
+REPO=$(git config remote.origin.url)
+
+# switch both git and https protocols as we don't know which travis is using
+REPO=${REPO/git:\/\/github.com\//git@github.com:}
+REPO=${REPO/https:\/\/github.com\//git@github.com:}
+
 export SSH_KEY="travis_ci_mlugs_website"
+chmod 600 $SSH_KEY
 eval `ssh-agent -s`
 ssh-add $SSH_KEY
-
-cd website
 git config user.name "travis"
 git config user.email "travis"
+
+cd website
 pelican -ds publishconf.py .
 echo "www.mlugs.de" > output/CNAME
 ghp-import output
-git push -f git@github.com:mlugs/website.git origin gh-pages
+git push -f $REPO gh-pages
