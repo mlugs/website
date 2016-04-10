43f5a6b2314b6cdd56d827403f375570bb1467bb
Andreas Madsack <andreas@madflex.de>
2016-04-10 16:20:48 +0200

    ssh file is in root folder

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+
+export SSH_KEY="travis_ci_mlugs_website"
+eval `ssh-agent -s`
+ssh-add $SSH_KEY
+
 cd website
 git config user.name "travis"
 git config user.email "travis"
 pelican -ds publishconf.py .
 echo "www.mlugs.de" > output/CNAME
-export SSH_KEY="travis_ci_mlugs_website"
-eval `ssh-agent -s`
-ssh-add $SSH_KEY
 ghp-import output
 git push -f git@github.com:mlugs/website.git origin gh-pages
