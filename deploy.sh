085b44dce46e48b722c4fb0f06537addd6e02ddc
Andreas Madsack <andreas@madflex.de>
2016-04-10 16:18:13 +0200

    next try

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,7 @@ git config user.email "travis"
 pelican -ds publishconf.py .
 echo "www.mlugs.de" > output/CNAME
 export SSH_KEY="travis_ci_mlugs_website"
+eval `ssh-agent -s`
+ssh-add $SSH_KEY
 ghp-import output
-git push origin gh-pages
+git push -f git@github.com:mlugs/website.git origin gh-pages
